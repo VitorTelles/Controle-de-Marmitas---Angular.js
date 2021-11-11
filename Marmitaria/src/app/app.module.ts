f8fae6c3d3c60cf8a50e3c3b8d9c1a7e368b2842
VitorTelles <vitortelles120599@gmail.com>
2021-11-11 14:55:38 -0300

    Começando a montar o layout do site

--- Marmitaria/src/app/app.module.ts
+++ Marmitaria/src/app/app.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { HeaderComponent } from './header/header.component';
-
 import { AppComponent } from './app.component';
-
-import { PackagesComponent } from './packages/packages.component';
 import { PackageListComponent } from './packages/package-list/package-list.component';
 import { PackageDetailsComponent } from './packages/package-details/package-details.component';
-import { PackageItemComponent } from './package/package-list/package-item/package-item.component';
-import { PackageLunchComponent } from './package-lunch/package-lunch.component';
-import { FoodListComponent } from './food-list/food-list.component';
 import { FoodEditComponent } from './food-list/food-edit/food-edit.component';
-
+import { FoodListComponent } from './food-list/food-list.component';
+import { PackagesComponent } from './packages/packages.component';
+import { PackageItemComponent } from './packages/package-list/package-item/package-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PackagesComponent,
     PackageListComponent,
     PackageDetailsComponent,
+    PackagesComponent,
     PackageItemComponent,
-    PackageLunchComponent,
-    FoodListComponent,
-    FoodEditComponent
+    FoodEditComponent,
+    FoodListComponent
   ],
   imports: [
     BrowserModule
