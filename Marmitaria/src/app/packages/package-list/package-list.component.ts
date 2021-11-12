e753f90d34c1261212a8746bf7f0373661675ffb
VitorTelles <vitortelles120599@gmail.com>
2021-11-12 10:20:22 -0300

    alterações nos componentes list

--- Marmitaria/src/app/packages/package-list/package-list.component.ts
+++ Marmitaria/src/app/packages/package-list/package-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./package-list.component.css']
 })
 export class PackageListComponent implements OnInit {
+
+  packages: Package[] = [
+    new Package('Churrasco', 'Carne de Picanha', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Freceitas.globo.com%2Fpicanha-na-brasa-gnt.ghtml&psig=AOvVaw0BmN3tAq2CdCB2AIRXE3ap&ust=1636741560477000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMDt0L73kPQCFQAAAAAdAAAAABAD')
+  ];
 
   constructor() { }
 
